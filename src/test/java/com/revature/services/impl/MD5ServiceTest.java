494618dd13fb03279ab013c0fe15d50039dcc43e
theo-gherman <61610977+theo-gherman@users.noreply.github.com>
2020-05-18 09:59:53 -0500

    created MD5 test
    created MD5 test

--- src/test/java/com/revature/services/impl/MD5ServiceTest.java
+++ src/test/java/com/revature/services/impl/MD5ServiceTest.java
@@ -1,5 +1,30 @@
 package com.revature.services.impl;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import com.revature.beans.Batch;
+import com.revature.beans.Reservation;
+import com.revature.beans.User;
+import com.revature.repositories.ReservationRepository;
+import com.revature.services.MD5Service;
+import com.revature.services.UserService;
+
 public class MD5ServiceTest {
+	
+	
+	
+	
+	@Test
+	public void testGetMD5() {
+		String mockInput = "testString";
+		String actual = MD5Service.getMd5(mockInput);
+		String expected = "536788f4dbdffeecfbb8f350a941eea3";
+		assertEquals(expected, actual);
+	}
 
 }
